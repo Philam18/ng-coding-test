d63da598385073982f14d2ea0523d75ce22891a9
Philip Lam <philiplam818@gmail.com>
2018-05-29 16:57:58 -0400

    turned http to https

--- src/app/restaurants.service.ts
+++ src/app/restaurants.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RestaurantsService {
-  url : string = "http://opentable.herokuapp.com/api/restaurants?city=";
+  url : string = "https://opentable.herokuapp.com/api/restaurants?city=";
 
   constructor(private http : HttpClient) { }
 
